f485b976fecc48846c29ffc92a522308c02e6d2c
Tho Nguyen <huu.tho.ns@gmail.com>
2022-07-07 10:14:26 +0700

    first commit

---
+++ src/components/Buttons/FullButton.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function FullButton({ title, action, border }) {
+  return (
+    <Wrapper
+      className="animate pointer radius8"
+      onClick={action ? () => action() : null}
+      border={border}
+    >
+      {title}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.button`
+  border: 1px solid ${(props) => (props.border ? "#707070" : "#2D812C")};
+  background-color: ${(props) => (props.border ? "transparent" : "#2D812C")};
+  width: 100%;
+  padding: 15px;
+  outline: none;
+  color: ${(props) => (props.border ? "#707070" : "#fff")};
+  :hover {
+    background-color: ${(props) => (props.border ? "transparent" : "#369735")};
+    border: 1px solid #2D812C;
+    color: ${(props) => (props.border ? "#2D812C" : "#fff")};
+  }
+`;
+
